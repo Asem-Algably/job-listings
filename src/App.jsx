b63b5ed53c87fe2390f7ae977eebcfeb185dba07
Asem Algably <asemalgably@gmail.com>
2024-09-19 17:17:17 +0200

    job elments are half done

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,35 @@ import { useState } from 'react';
 import data from '../data.json'
 import './App.css';
 
-function Job({position}) {
+function Job(props
+) {
   return(
-      <h1>{position}</h1>
+      <div className='job'>
+      <img src={`.${props.logo}`} alt={`${props.company} logo`} className='logo'/>
+      <div className='info'>
+      <div className="main_tags">
+        <h3>{props.company}</h3>
+        <h4>{props.newchance?"new!":null}</h4>
+        <h4>{props.featured?"featured!":null}</h4>
+      </div>
+
+      <h2>{props.position}</h2>
+
+      <div className="subinfo">
+        <span>{props.location}</span>
+        <span>.</span>
+        <span>{props.postedAt}</span>
+        <span>.</span>
+        <span>{props.contract}</span>
+      </div>
+      </div>
+      <div className='tablets'>
+        <p>{props.level}</p>
+        <p>{props.role}</p>
+        {props.languages && props.languages.map(lang => <span key={lang}>{lang} </span>)}
+        {props.tools && props.tools.map(tool => <span key={tool}>{tool} </span>)}
+      </div>
+      </div>
   )
 }
 
@@ -20,13 +46,26 @@ function JobsWrapper({data}) {
   return (
     <>
       {data.map((job, index) => (
-        <Job key={index} position={job.position} />
+        <Job key={index}
+        company={job.company}
+        logo={job.logo}
+        newchance={job.newchance}
+        featured={job.featured}
+        position={job.position}
+        role={job.role}
+        level={job.level}
+        postedAt={job.postedAt}
+        contract={job.contract}
+        location={job.location}
+        languages={job.languages}
+        tools={job.tools}
+        />
       ))}
     </>
   );
 }
 
-function App() {
+function App(className=appclass) {
   const [keywords, setkeywords] = useState([]);
   const jobsdata = data;
 
